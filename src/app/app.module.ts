829cc9e0dba3bea34f4633555671a36d400ac2e7
SHIV220 <55434267+SHIV220@users.noreply.github.com>
2023-05-30 19:32:34 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }  from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+
+@NgModule({
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent,CalculatorComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
